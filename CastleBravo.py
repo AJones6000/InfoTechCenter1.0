69aaae2e41c48aa3facdf6c61793a607b2044fcc
Jones <alexjones4053@gmail.com>
2024-10-08 08:26:40 -0400

    Added A While Loop For Our Os Booting Up

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,1 +1,18 @@
-print("\nWelcome to InfoTechCenter")
+import sys
+import time
+
+print("\n\tWelcome to InfoTechCenter\n")
+
+
+x=0
+ellipsis = 0
+while x != 20:
+    x += 1
+    message = ("Infotech Center System Booting"+ "."* ellipsis)
+    ellipsis += 1
+    sys.stdout.write("\r" + message)
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x ==20:
+        print("\n\nOperating System Booted Up")
