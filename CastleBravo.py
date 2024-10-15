fb04a4b1c7bd6055bcf1801dee76cd3a8b6847d5
Jones <alexjones4053@gmail.com>
2024-10-15 07:53:45 -0400

    Added An ELIF For Blizzard Condition

--- CastleBravo.py
+++ CastleBravo.py
@@ -17,5 +17,14 @@ def vehicaleResponseSysterm():
     if WeatherAlert == "Snowy":
         print("\nThe National Weather Service Updated Our Alarm By 30 Minutes Because "
               "Of The Forcast Of",WeatherAlert, "Weather Condition.")
+        sleep(1)
+        print("\nVRS System Has Been Engaged Only Allowing To Drive 55MPH")
+    elif WeatherAlert == "Blizzard":
+        print("\nThe National Weather Service Updated Our Alarm By 45 Minutes Because "
+              "Of The Forcast Of", WeatherAlert, "Weather Condition.")
+        sleep(1)
+        print("\nVRS System Has Been Engaged Only Allowing To Drive 45MPH")
+
+
 
 vehicaleResponseSysterm()
