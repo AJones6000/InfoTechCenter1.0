4860301832b85d5af198dbf3a6c4842e321eb4a5
Jones <alexjones4053@gmail.com>
2024-10-15 08:08:42 -0400

    Added More ELIF And A ELSE

--- CastleBravo.py
+++ CastleBravo.py
@@ -24,6 +24,26 @@ def vehicaleResponseSysterm():
               "Of The Forcast Of", WeatherAlert, "Weather Condition.")
         sleep(1)
         print("\nVRS System Has Been Engaged Only Allowing To Drive 45MPH")
+    elif WeatherAlert == "Rainy":
+        print("\nThe National Weather Service Updated Our Alarm By 15 Minutes Because "
+              "Of The Forcast Of", WeatherAlert, "Weather Condition.")
+        sleep(1)
+        print("\nVRS System Has Been Engaged Only Allowing To Drive 65MPH")
+    elif WeatherAlert == "Windy":
+        print("\nThe National Weather Service Updated Our Alarm By 5 Minutes Because "
+              "Of The Forcast Of", WeatherAlert, "Weather Condition.")
+        sleep(1)
+        print("\nVRS System Has Been Engaged Only Allowing To Drive 70MPH")
+    elif WeatherAlert == "Icy":
+        print("\nThe National Weather Service Updated Our Alarm By 50 Minutes Because "
+              "Of The Forcast Of", WeatherAlert, "Weather Condition.")
+        sleep(1)
+        print("\nVRS System Has Been Engaged Only Allowing To Drive 30MPH")
+    else:
+        print("The National Weather Service Is Calling For",WeatherAlert," Skies")
+        sleep(1)
+        print("VRS Has Been Disabled")
+
 
 
 
